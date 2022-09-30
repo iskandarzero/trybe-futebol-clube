d08a80aaaff3308809895712ea4573573a21b57d
iskandarzero <riderzerotg@gmail.com>
2022-09-30 03:20:01 -0300

    feat: user model

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,38 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER
+  },
+  username: {
+    allowNull: false,
+    type: STRING
+  },
+  role: {
+    allowNull: false,
+    type: STRING
+  },
+  email: {
+    allowNull: false,
+    type: STRING
+  },
+  password: {
+    allowNull: false,
+    type: STRING
+  }
+}, {
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+})
+export default User;
