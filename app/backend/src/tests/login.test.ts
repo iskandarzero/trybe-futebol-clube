ac59ad4ae0b04cacd4bbd76c1aebfb128153bd1b
iskandarzero <riderzerotg@gmail.com>
2022-09-30 17:10:36 -0300

    feat: adds some tests

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,52 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/UserModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Rota POST /login', () => {
+  describe('quando os dados do `body` são válidos', () => {
+    let postLogin: Response;
+
+    before(async () => {
+      sinon
+        .stub(User, "findOne")
+        .resolves({
+          id: 1,
+          username: 'User',
+          role: 'user',
+          email: "user@user.com",
+          password: "banana"
+        } as User);
+
+      try {
+        postLogin = await chai.request(app)
+          .post('/login')
+          .send({
+            email: "user@user.com",
+            password: "banana"
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+    
+    after(()=>{
+      (User.findOne as sinon.SinonStub).restore();
+    })
+
+    it('retorna 200', async () => {
+      const { status } = postLogin;
+
+      expect(status).to.be.equals(200);
+    })
+  })
+});
