634a91b8ddb9a5d908537e306bc767c1e7459062
iskandarzero <riderzerotg@gmail.com>
2022-10-01 08:32:15 -0300

    feat: adds middleware to validate login

--- app/backend/src/controllers/user.controller.ts
+++ app/backend/src/controllers/user.controller.ts
@@ -14,9 +14,9 @@ export default class UserController {
   }
 
   public validate = (req: Request, res: Response, next: NextFunction) => {
-    const {email} = req.body;
+    const {email, password} = req.body;
 
-    if (!email) return res.status(400).json({ message: "All fields must be filled" });
+    if (!email || !password) return res.status(400).json({ message: "All fields must be filled" });
 
     next();
   }
