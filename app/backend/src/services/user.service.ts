a320ee12a8840633b178f776946cef39ff5313db
iskandarzero <riderzerotg@gmail.com>
2022-10-01 06:36:29 -0300

    feat: user services

---
+++ app/backend/src/services/user.service.ts
@@ -0,0 +1,22 @@
+import { generateToken } from '../auth/token';
+import User from '../database/models/UserModel'
+import Login from '../interfaces/login.interface';
+const bcrypt = require('bcryptjs');
+
+export default class UserService {
+  public async login(user: Login) {
+    console.log(user);
+    
+
+    const userInfo = await User.findOne({ where: { email: user.email } });
+
+    if (!userInfo) return { code: 400, message: 'generica' };
+
+    const comparePasswords = await bcrypt.compare(user.password, userInfo.password);
+    if (!comparePasswords) return { code: 400, message: 'generica2' };
+
+    const token = await generateToken(user);
+    
+    return {code: 200, token};
+  }
+}
