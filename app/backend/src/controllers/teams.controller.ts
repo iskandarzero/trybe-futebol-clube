0c3bb77db60c4a09fc61977d9d1faf8fb41beb2d
iskandarzero <riderzerotg@gmail.com>
2022-10-03 18:57:01 -0300

    feat: getById controller

--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -4,10 +4,17 @@ import { Request, Response, NextFunction } from 'express';
 export default class TeamsController {
   constructor(private _teamsService = new TeamsService()) {}
 
-  public getAll = async (req: Request, res: Response) => {
+  public getAll = async (_req: Request, res: Response) => {
     const teams = await this._teamsService.getAll();
     console.log(teams);
 
     res.status(200).json(teams);
+  }
+
+  public getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const team = await this._teamsService.getByid(Number(id));
+
+    res.status(200).json(team);
   }
 }
