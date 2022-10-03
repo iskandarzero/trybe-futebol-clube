e2a5d1e51b14351d99b5d2d3df31a54874d1aa58
iskandarzero <riderzerotg@gmail.com>
2022-10-03 03:15:28 -0300

    feat: func for validate user role

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,13 +1,10 @@
-import { generateToken } from '../auth/token';
+import { decodeToken, generateToken } from '../auth/token';
 import User from '../database/models/user.model'
 import Login from '../interfaces/login.interface';
 const bcrypt = require('bcryptjs');
 
 export default class UserService {
   public async login(user: Login) {
-    console.log(user);
-    
-
     const userInfo = await User.findOne({ where: { email: user.email } });
 
     if (!userInfo) return { code: 401, message: 'Incorrect email or password' };
@@ -15,8 +12,18 @@ export default class UserService {
     const comparePasswords = await bcrypt.compare(user.password, userInfo.password);
     if (!comparePasswords) return { code: 400, message: 'Incorrect email or password' };
 
-    const token = await generateToken(user);
+    const token = generateToken(user);
     
     return {code: 200, token};
+  }
+
+  public async validate(token: string) {
+    const { email } = decodeToken(token);
+    
+    if (!email) return {code: 404, message: 'Invalid token'}
+
+    const userRole = await User.findOne({attributes: ['role']})
+
+    return {code: 200, message: userRole};
   }
 }
