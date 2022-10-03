3ea5c070f1a6fd64b49028393b5db96a67e3c490
iskandarzero <riderzerotg@gmail.com>
2022-10-03 17:51:38 -0300

    feat: team service

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -1,1 +1,9 @@
 import Teams from '../database/models/teams.model';
+
+export default class TeamsService {
+  public async getAll() {
+    const result = await Teams.findAll();
+
+    return result;
+  }
+}
