1f7c449cf58183fe626188e994971cc44bfcc9b6
iskandarzero <riderzerotg@gmail.com>
2022-10-04 08:38:30 -0300

    feat: matches route

---
+++ app/backend/src/routes/matches.route.ts
@@ -0,0 +1,10 @@
+import MatchesController from '../controllers/matches.controller';
+import { Router } from 'express';
+
+const matchesRoute = Router();
+
+const matchesController = new MatchesController();
+
+matchesRoute.get('/', matchesController.getAll)
+
+export default matchesRoute;
