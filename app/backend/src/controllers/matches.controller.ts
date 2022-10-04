5f29404e012605e5904b0749b0661f9233b9b888
iskandarzero <riderzerotg@gmail.com>
2022-10-04 19:10:12 -0300

    feat: createMatch func on controller

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -7,16 +7,21 @@ export default class MatchesController {
   public getAll = async (req: Request, res: Response) => {
     const search = req.query.inProgress;
     let matches;
-    const isTrueSet = (search === 'true');
     
-
     if(search) {
-      console.log('entrou');
+      const isTrueSet = (search === 'true');
       matches = await this._matchesService.getByProgress(isTrueSet);
     } else {
       matches = await this._matchesService.getAll();
     }
 
     res.status(200).json(matches);
+  }
+
+  public createMatch = async (req: Request, res: Response) => {
+    const matchInfo = req.body;
+    const match = await this._matchesService.createMatch(matchInfo);
+
+    res.status(201).json(match);
   }
 }
