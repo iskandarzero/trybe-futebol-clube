2f43c6a4849acde11138361eb2e475e6ed3e75e6
iskandarzero <riderzerotg@gmail.com>
2022-10-04 20:13:57 -0300

    feat: use middleware

--- app/backend/src/routes/matches.route.ts
+++ app/backend/src/routes/matches.route.ts
@@ -6,7 +6,7 @@ const matchesRoute = Router();
 const matchesController = new MatchesController();
 
 matchesRoute.get('/', matchesController.getAll);
-matchesRoute.post('/', matchesController.createMatch);
+matchesRoute.post('/', matchesController.validateMatch ,matchesController.createMatch);
 matchesRoute.patch('/:id/finish', matchesController.finishMatch);
 
 export default matchesRoute;
