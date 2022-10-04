746db55588075c9d752f8e01e8edec6e696eebfd
iskandarzero <riderzerotg@gmail.com>
2022-10-04 17:57:59 -0300

    reafctor: function getAll

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -4,8 +4,18 @@ import { Request, Response } from 'express';
 export default class MatchesController {
   constructor(private _matchesService = new MatchesService()) {}
 
-  public getAll = async (_req: Request, res: Response) => {
-    const matches = await this._matchesService.getAll();
+  public getAll = async (req: Request, res: Response) => {
+    const search = req.query.inProgress;
+    let matches;
+    const isTrueSet = (search === 'true');
+    
+
+    if(search) {
+      console.log('entrou');
+      matches = await this._matchesService.getByProgress(isTrueSet);
+    } else {
+      matches = await this._matchesService.getAll();
+    }
 
     res.status(200).json(matches);
   }
